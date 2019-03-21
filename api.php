434f4ab986dfdea60412f88c2990aaea12d11b5a
Alex <alexcuevitas@gmail.com>
2019-03-21 19:18:11 +0100

    token

---
+++ api.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexc
+ * Date: 21/03/2019
+ * Time: 19:12
+ */
+
+require_once('token.php');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type,
+Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+/*SI NO HAY TOKEN, CREAMOS EL TOKEN CON LA INFO DEL USUARIO*/
+$data = file_get_contents('php://input'); //$data = {“nom”:”juan”,”pass”:”1234}
+$token =jwtGetCodeJSON($data);
+echo '{"token":"'.$token.'"}';
+$arrayAssoc = json_decode($data);
+echo "nom: ". $arrayAssoc->nom;
+
+
+?>
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 echo '{"username" : user, "password" : pass}'
 
-
+// idea: guardar una palabra clave para cada user (hash??) en la bdd
 ?>

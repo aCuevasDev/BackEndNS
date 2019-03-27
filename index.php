0d978330bebc9984a52db2a1dd4c98ba6a771f43
Alex <alexcuevitas@gmail.com>
2019-03-28 00:06:39 +0100

    test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-echo '{"username" : user, "password" : pass}'
+echo '{"username" : user, "password" : pass}';
 
+echo "test";
 // idea: guardar una palabra clave para cada user (hash??) en la bdd
 ?>

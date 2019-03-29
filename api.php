619e50f87be2e3185a9d8ff3c4c412ac0465ff91
Alex <alexcuevitas@gmail.com>
2019-03-29 16:34:39 +0100

    composer

--- api.php
+++ api.php
@@ -14,10 +14,25 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 /*SI NO HAY TOKEN, CREAMOS EL TOKEN CON LA INFO DEL USUARIO*/
 $data = file_get_contents('php://input'); //$data = {“nom”:”juan”,”pass”:”1234}
-$token =jwtGetCodeJSON($data);
-echo '{"token":"'.$token.'"}';
-$arrayAssoc = json_decode($data);
-echo "nom: ". $arrayAssoc->nom;
+$token = jwtGetCodeJSON($data);
+echo '{"token":"' . $token . '"}';
+//$arrayAssoc = json_decode($data);
+//echo "nom: ". $arrayAssoc->nom;
+//THIS SHOULD BE COMMENTED I THINK
+
+function checkAuthToken()
+{
+    $headers = apache_request_headers();
+    if (isset($headers["Authorization"]) && $headers["Authorization"] != "") {
+        $token_recibido = $headers["Authorization"];
+        if (jwtCheckCodeJSON($token_recibido)) {//SI HAY TOKEN, MIRAMOS SI ES CORRECTO
+// podemos extraer el encabezado, el contenido y la signatura.
+            $jwt_values = explode('.', $jwt_token);
+            $payload = base64_decode($jwt_values[1]);
+            return json_encode($payload);
+        }
+    }
+}
 
 
 ?>

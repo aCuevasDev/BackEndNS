e115481d689faaa01960fe1ea393d5edac66085f
Alex <alexcuevitas@gmail.com>
2019-04-19 10:19:42 +0200

    .

--- register.php
+++ register.php
@@ -1,19 +1,19 @@
 <?php
 
-    $paramMap = $request->getParsedBody();
-    if ($paramMap['email'] == null ||
-        $paramMap['username'] == null ||
-        $paramMap['password'] == null) {
-        return $response->withJson([], 404);
-    }
-    $email = $paramMap['email'];
-    $username = $paramMap['username'];
-    $password = $paramMap['password'];
-    $user = new \API\Model\User();
-    $user->setUsername($paramMap['username']);
-    $user->setPassword($paramMap['password']);
-    $user->setEmail($paramMap['email']);
-    $dateTime = new DateTime();
-    $user->setCreated($dateTime->getTimestamp());
-    $user->save();
-    echo $response->withJson($user, 200);
+$paramMap = $_GET;
+if ($paramMap['email'] == null ||
+    $paramMap['username'] == null ||
+    $paramMap['password'] == null) {
+    echo $response->withJson([], 404);
+}
+$email = $paramMap['email'];
+$username = $paramMap['username'];
+$password = $paramMap['password'];
+$user = new \API\Model\User();
+$user->setUsername($paramMap['username']);
+$user->setPassword($paramMap['password']);
+$user->setEmail($paramMap['email']);
+$dateTime = new DateTime();
+$user->setCreated($dateTime->getTimestamp());
+$user->save();
+echo $response->withJson($user, 200);
